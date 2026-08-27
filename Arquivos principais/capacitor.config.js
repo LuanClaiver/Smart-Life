511bdd780cf949775b24cf33f5076e14f1b22b68
LuanClaiver <luanclaivermpp@gmail.com>
2026-08-26 21:34:25 -0300

    Smart Life v0.5.6

---
+++ Arquivos principais/capacitor.config.js
@@ -0,0 +1,1 @@
+module.exports={appId:'com.smartlife.app',appName:'Smart Life',webDir:'www',bundledWebRuntime:false,plugins:{LocalNotifications:{smallIcon:'ic_stat_smart_life',iconColor:'#23F082'}}};
