fc9017fb7b46c61c63d78c9bc48f8c8fc2808893
LuanClaiver <luanclaivermpp@gmail.com>
2026-08-30 17:14:23 -0300

    Smart Life v0.7.8

---
+++ Iniciar Aplicacao.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal EnableExtensions
+title Smart Life v0.7.8
+cd /d "%~dp0"
+
+set "APP_FILE=%~dp0Arquivos principais\www\index.html"
+
+if not exist "%APP_FILE%" (
+  echo ERRO: O arquivo principal do Smart Life nao foi encontrado.
+  echo Mantenha este BAT na raiz do repositorio, ao lado da pasta Arquivos principais.
+  pause
+  exit /b 1
+)
+
+start "" "%APP_FILE%"
+endlocal
+exit /b 0
